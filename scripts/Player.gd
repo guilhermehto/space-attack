9498b871e331251df00adf85ab1940ec2c8dfe9d
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-05 23:51:05 -0300

    Shooting

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,10 +2,15 @@ extends KinematicBody2D
 
 const SPEED = 500
 
+export(PackedScene) var projectile
+
 onready var sprite = $Sprite
+onready var timer = $Timer
+
 
 var screen_size
 var half_sprite_size
+var can_shoot = true
 
 
 func _ready():
@@ -18,4 +23,14 @@ func _process(delta):
 	elif Input.is_action_pressed("right"):
 		position.x += SPEED * delta
 	
+	if can_shoot and Input.is_action_pressed("shoot"):
+		can_shoot = false
+		var new_projectile = projectile.instance()
+		get_parent().add_child(new_projectile)
+		new_projectile.position = position
+		timer.start()
+	
 	position.x = clamp(position.x, 0 + half_sprite_size, screen_size - half_sprite_size)
+
+func _on_Timer_timeout():
+	can_shoot = true
